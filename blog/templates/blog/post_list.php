1d6b207b632900a96c2d0478c680485d1c7d7444
Vinse <flores.majano@gmail.com>
2017-06-30 15:09:39 -0600

    Plantilla modificada obteniendo informacion de BDs atraves de los modelso

--- blog/templates/blog/post_list.php
+++ blog/templates/blog/post_list.php
@@ -5,33 +5,16 @@
 </head>
 <body>
   <div>
-                <h1><a href="">Taller Django</a></h1>
-            </div>
-
-            <div>                
-                <h2><a href="">Primer blog usando Python</a></h2>
-                <p>
-                	Python puede ser utilizado en diversas plataformas y sistemas operativos, entre los que podemos destacar los más populares, cómo Windows, Mac OS X y Linux. Pero, además, Python también puede funcionar en smartphones, Nokia desarrolló un intérprete de esté lenguaje para su sistema operativo Symbian.
-                </p>
-                <p>Fecha de publicacion:
-                	<script type="text/javascript">
-                		var f = new Date();
-						document.write(f.getDate() + "/" + (f.getMonth() +1) + "/" + f.getFullYear());
-                	</script>
-                </p>
-            </div>
-
-            <div>                
-                <h2><a href="">Segundo blog usando el Framework Django</a></h2>
-                <p>¿Cuántas personas no se han preguntado lo mismo?, antes de responder esto deberíamos comenzar por otra pregunta, si es que aún no la conocen. ¿Que es Django?.
-
-				Los desarrolladores de Django lo definen así: “Django es un framework web de alto nivel que fomenta el desarrollo rápido y el diseño limpio y pragmático”, algo muy prometedor, pero  ¿Cuán cierta es esta definición? Para responder esto vamos a entrar un poco más en este framework así que explicaremos un poco sus características, así que, mientras vayas leyendo no saques de tu mente esta definición.</p>
-				<p>Fecha de publicacion:
-                	<script type="text/javascript">
-                		var f = new Date();
-						document.write(f.getDate() + "/" + (f.getMonth() +1) + "/" + f.getFullYear());
-                	</script>
-                </p>
-            </div>
+  	<h1><a href="">Taller Django</a></h1>
+  </div>
+ 	{% for post in posts %}
+ 		<div> 			
+ 			<h1><a href="">{{ post.title }}</a></h1>
+ 			<p>Publicado: {{ post.published_date }}</p>
+ 			<p>{{ post.text|linebreaksbr }}</p>
+ 		</div>
+    	
+    {% endfor %}
+          
 </body>
 </html>
