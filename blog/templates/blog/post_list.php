f2d02308cf395e49b01a71838a8a655d4b733c1c
Vinse <flores.majano@gmail.com>
2017-06-30 15:54:57 -0600

    Uso de css

--- blog/templates/blog/post_list.php
+++ blog/templates/blog/post_list.php
@@ -1,20 +1,32 @@
+{% load staticfiles %}
 <!DOCTYPE html>
 <html>
 <head>
 	<title>DESARROLLO WEB</title>
+	 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="{% static 'css/blog.css' %}">
+    <link href="https://fonts.googleapis.com/css?family=Lobster&subset=latin,latin-ext" rel="stylesheet" type="text/css">
 </head>
 <body>
-  <div>
+  <div class="page-header">
   	<h1><a href="">Taller Django</a></h1>
   </div>
- 	{% for post in posts %}
- 		<div> 			
- 			<h1><a href="">{{ post.title }}</a></h1>
- 			<p>Publicado: {{ post.published_date }}</p>
- 			<p>{{ post.text|linebreaksbr }}</p>
- 		</div>
-    	
-    {% endfor %}
+     <div class="content container">
+        <div class="row">
+            <div class="col-md-8">
+                {% for post in posts %}
+                    <div class="post">
+                        <div class="date">
+                            {{ post.published_date }}
+                        </div>
+                        <h1><a href="">{{ post.title }}</a></h1>
+                        <p>{{ post.text|linebreaksbr }}</p>
+                    </div>
+                {% endfor %}
+            </div>
+        </div>
+    </div>
           
 </body>
 </html>
