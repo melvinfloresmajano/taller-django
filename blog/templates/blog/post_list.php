4706b9b1bc1309d94044a452ac678628f362d8c5
Vinse <flores.majano@gmail.com>
2017-07-01 08:58:28 -0600

    añadiendo nuevas vistas

--- blog/templates/blog/post_list.php
+++ blog/templates/blog/post_list.php
@@ -1,32 +1,12 @@
-{% load staticfiles %}
-<!DOCTYPE html>
-<html>
-<head>
-	<title>DESARROLLO WEB</title>
-	 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
-    <link rel="stylesheet" href="{% static 'css/blog.css' %}">
-    <link href="https://fonts.googleapis.com/css?family=Lobster&subset=latin,latin-ext" rel="stylesheet" type="text/css">
-</head>
-<body>
-  <div class="page-header">
-  	<h1><a href="">Taller Django</a></h1>
-  </div>
-     <div class="content container">
-        <div class="row">
-            <div class="col-md-8">
-                {% for post in posts %}
-                    <div class="post">
-                        <div class="date">
-                            {{ post.published_date }}
-                        </div>
-                        <h1><a href="">{{ post.title }}</a></h1>
-                        <p>{{ post.text|linebreaksbr }}</p>
-                    </div>
-                {% endfor %}
+  {% extends 'blog/base.html' %}
+    {% block content %}
+        {% for post in posts %}
+            <div class="post">
+                <div class="date">
+                    {{ post.published_date }}
+                </div>
+                <h1><a href="{% url 'blog.views.post_detail' pk=post.pk %}">{{ post.title }}</a></h1>
+                <p>{{ post.text|linebreaksbr }}</p>
             </div>
-        </div>
-    </div>
-          
-</body>
-</html>
+        {% endfor %}
+    {% endblock content %}
